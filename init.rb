1dbe3b381df928f86e0e8f3e290043968e99dd4b
ian eyberg <ian@seeinginteractive.com>
2010-10-19 16:52:47 -0500

    adding in initial lib and shit; just trying to get listed first

---
+++ init.rb
@@ -0,0 +1,5 @@
+require File.dirname(__FILE__) + '/lib/honsole'
+
+Heroku::Command::Help.group("Honsole") do |group|
+  group.command "honsole",                "pop a multi-line enabled console"
+end
