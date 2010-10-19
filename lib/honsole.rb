90e8d403c8c5b57eebc1321a6121e83f9cce1934
ian eyberg <ian@seeinginteractive.com>
2010-10-19 17:11:16 -0500

    obv dont know how to install local copy yet

--- lib/honsole.rb
+++ lib/honsole.rb
@@ -1,1 +1,36 @@
 puts 'test'
+
+module Heroku::Command
+
+  class App
+    # set option to color output
+    def console_with_colorize
+      puts 'piss your pants'
+      console_without_colorize
+    end
+
+    alias_method :console_without_colorize, :console
+    alias_method :console, :console_with_colorize
+
+  end
+end
+
+=begin
+module Heroku::Command
+  class App < Base
+ 
+def console_session(app)
+  heroku.console(app) do |console|
+    console_history_read(app)
+
+    display "Ruby console for #{app}.#{heroku.host}"
+    while cmd = Readline.readline('>> ')
+      unless cmd.nil? || cmd.strip.empty?
+        console_history_add(app, cmd)
+        break if cmd.downcase.strip == 'exit'
+        display console.run(cmd)
+      end
+    end
+  end
+end
+=end
