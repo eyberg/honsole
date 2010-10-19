b1e7da94a0597d9f798ec88ab68957d84afdcc3a
ian eyberg <ian@seeinginteractive.com>
2010-10-19 17:35:29 -0500

    trying out cmd substitution for multi-line support

--- lib/honsole.rb
+++ lib/honsole.rb
@@ -1,21 +1,18 @@
-puts 'stest'
-
 module Heroku::Command
   class App < Base
 
     def honsole
-      puts 'lla'
       console
     end
 
- 
     def console_session(app)
       heroku.console(app) do |console|
         console_history_read(app)
 
-        display "Ruby t console for #{app}.#{heroku.host}"
+        display "Ruby console for #{app}.#{heroku.host}"
         while cmd = Readline.readline('>> ')
           unless cmd.nil? || cmd.strip.empty?
+            cmd = cmd.gsub("\n", ";")
             console_history_add(app, cmd)
             break if cmd.downcase.strip == 'exit'
             display console.run(cmd)
@@ -40,26 +37,6 @@ module Heroku::Command
     alias_method :console_without_colorize, :console
     alias_method :console, :console_with_colorize
 
-  end
-end
-=end
-
-=begin
-module Heroku::Command
-  class App < Base
- 
-def console_session(app)
-  heroku.console(app) do |console|
-    console_history_read(app)
-
-    display "Ruby console for #{app}.#{heroku.host}"
-    while cmd = Readline.readline('>> ')
-      unless cmd.nil? || cmd.strip.empty?
-        console_history_add(app, cmd)
-        break if cmd.downcase.strip == 'exit'
-        display console.run(cmd)
-      end
-    end
   end
 end
 =end
